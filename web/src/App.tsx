53d7ccf5aec39c596b7f50827364b32a3ffc74d4
victor-medeiros <victor.msilva@bandtec.com.br>
2020-10-12 19:57:59 -0300

    Estilização da landing page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,32 @@
 import React from 'react';
 
+import './styles/global.css';
+import './styles/pages/landing.css';
+
+import { FiArrowRight } from 'react-icons/fi';
+
+import logoImg from './images/logo.svg';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello happy! :)</h1>
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt="Happy"/>
+
+        <main>
+          <h1>Leve a felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças</p>
+        </main>
+
+        <div className="location">
+          <strong>São Paulo</strong>
+          <span>São Paulo</span>
+        </div>
+
+        <a href="" className="enter-app">
+          <FiArrowRight color="rgba(0, 0, 0, 0.6)" size={26} />
+        </a>
+      </div>
     </div>
   );
 }
