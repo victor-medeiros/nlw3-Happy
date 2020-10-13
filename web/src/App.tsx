d66e4033547d00d0da57fd88b85649bdb6e408f8
victor-medeiros <victor.msilva@bandtec.com.br>
2020-10-12 22:18:12 -0300

    Configuração das rotas

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -6,28 +6,11 @@ import './styles/pages/landing.css';
 import { FiArrowRight } from 'react-icons/fi';
 
 import logoImg from './images/logo.svg';
+import Routes from './routes';
 
 function App() {
   return (
-    <div id="page-landing">
-      <div className="content-wrapper">
-        <img src={logoImg} alt="Happy"/>
-
-        <main>
-          <h1>Leve a felicidade para o mundo</h1>
-          <p>Visite orfanatos e mude o dia de muitas crianças</p>
-        </main>
-
-        <div className="location">
-          <strong>São Paulo</strong>
-          <span>São Paulo</span>
-        </div>
-
-        <a href="" className="enter-app">
-          <FiArrowRight color="rgba(0, 0, 0, 0.6)" size={26} />
-        </a>
-      </div>
-    </div>
+    <Routes />
   );
 }
 
---
+++ web/src/pages/Landing.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import '../styles/global.css';
+import '../styles/pages/landing.css';
+
+import { FiArrowRight } from 'react-icons/fi';
+
+import logoImg from '../images/logo.svg';
+
+function Landing() {
+  return (
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt="Happy"/>
+
+        <main>
+          <h1>Leve a felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças</p>
+        </main>
+
+        <div className="location">
+          <strong>São Paulo</strong>
+          <span>São Paulo</span>
+        </div>
+
+        <Link to="/app" className="enter-app">
+          <FiArrowRight color="rgba(0, 0, 0, 0.6)" size={26} />
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Landing;
